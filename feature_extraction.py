d4ff02678a98a8c83174454311a743451ecb03be
Vinay Hiremath <vhiremath95@gmail.com>
2016-04-05 18:47:15 -0400

    Added some comments

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# Returns unigrams, bigrams, trigrams dicts
+# For example, `trigrams[1] = ['house', 'is', 'nice']`
 def get_grams_indices(tokens):
     unigrams, bigrams, trigrams = [], [], []
     for i in range(len(tokens)):
@@ -10,6 +12,10 @@ def get_grams_indices(tokens):
         unigrams[i] = tokens[i]
     return unigrams, bigrams, trigrams
 
-def extract_features(tokens):
-    unigrams, bigrams, trigrams = get_grams_indices(tokens)
-    # Need to extract features (length of word, entropy, etc.)
+# Takes as input a list of doc tokens (nested list)
+# For instance, with two docs: [[token_1, token_2], [token_3, token_4, token_5]]
+def extract_features(doc_tokens):
+    for tokens in doc_tokens:
+        unigrams, bigrams, trigrams = get_grams_indices(tokens)
+        # Need to extract features (length of word, entropy, etc.)
+    return features # Same length as doc_tokens (number of docs)
