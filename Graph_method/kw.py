5b471720b37a38d99f025f7d96a75ed2affc04ab
sjmsjj <jmsang@umich.edu>
2016-04-05 16:31:18 -0400

    Added files via upload

--- Graph_method/kw.py
+++ Graph_method/kw.py
@@ -1,8 +1,9 @@
 """
 By Jianming Sang 04/2016
 
+
 Main idea:
-    Graph based keywords extraction
+    Graph based unsupervised algorithm for keywords extraction
     To build the graph, use terms as vertex and co-occurrence relation between terms as edges
     Use Closeness centrality and TextRank centrality to rank the vertex(terms), respectively
     Pick the k terms with highest centrality values as the key words 
@@ -15,11 +16,16 @@ Work flow:
     Calculate the centrality values based on different protocols for each vertex in the graph
     Extrac k tokens with highest centrality value as the key words
     Calcualte the accuracy and recall using the given keywords and the extracted keywords
+
+To extract keywords, class Closeness or class TextRank can be used 
+Class Solution is implemented to speed up the process to get accuracy from a lot of files by using multiprocessing programming
 """
 
 import re
 import os
 import sys
+import threading
+from multiprocessing import Process, Queue, cpu_count
 from collections import defaultdict, deque
 import PorterStemmer as stem  
 
@@ -66,6 +72,7 @@ class Keyword(object):
         self.get_items()
 
     def fit(self, filename, ans_file):
+        print filename
         self.get_keywords(filename)
         self.get_ans(ans_file)
         
@@ -84,6 +91,7 @@ class Keyword(object):
         for word in self.kw:
             if word in self.ans:
                 hit += 1
+        print len(self.kw), len(self.ans)
         return hit/len(self.kw), hit/len(self.ans)
 
 
@@ -94,11 +102,14 @@ class Graph(object):
 
     def build_co_occurrence(self):
         self.co_occurrence = dict()
+        self.neighbors = defaultdict(list)
         for sentence in self.sentence_list:
             for w1 in sentence[:-1]:
                 for w2 in sentence[1:]:
                     if w1 != w2:
                         self.co_occurrence[(w1, w2)] = self.co_occurrence.get((w1, w2), 0) + 1
+                        self.neighbors[w1].append(w2)
+                        self.neighbors[w2].append(w1)
 
     def build_undirected_weighted_edges(self):
         self.edges = dict()
@@ -106,9 +117,9 @@ class Graph(object):
             for w2 in self.items:
                 if w1 != w2:
                     if (w1, w2) in self.co_occurrence or (w2, w1) in self.co_occurrence:
-                        self.edges[(w1, w2)] = 1.0/(self.co_occurrence.get((w1, w2), 0) + self.co_occurrence.get((w2, w1), 0))
+                        self.edges[(w1, w2)] = self.co_occurrence.get((w1, w2), 0) + self.co_occurrence.get((w2, w1), 0)
                     else:
-                        self.edges[(w1, w2)] = 100
+                        self.edges[(w1, w2)] = 0.001  #build a edge with small weight if two words do not co-occurrence
 
 class Closeness(Keyword, Graph):
     def get_shortest_path(self, root):   #using the Dijkstra's algorithm to get the shortest path from root node to all the other nodes
@@ -122,12 +133,11 @@ class Closeness(Keyword, Graph):
         while queue:
             item = min(queue.items(), key = lambda item: item[1])[0]
             del queue[item]
-            for key in self.items:
-                if key != item:
-                    temp = self.dist[item] + self.edges[(item, key)]
-                    if temp < self.dist[key]:
-                        self.dist[key] = temp
-                        queue[key] = temp
+            for key in self.neighbors[item]:
+                temp = self.dist[item] + 1.0/self.edges[(item, key)]
+                if temp < self.dist[key]:
+                    self.dist[key] = temp
+                    queue[key] = temp
 
     def get_keywords(self, filename):
         self.kw = []
@@ -136,7 +146,7 @@ class Closeness(Keyword, Graph):
         self.build_undirected_weighted_edges()
         n = len(self.items)
         score = dict()
-        rank = len(self.items)/3
+        rank = len(self.items)/3   #get the top 1/3 as the potential keywords
         for item in self.items:
             self.get_shortest_path(item)
             score[item] = (n - 1) * 1.0 / sum(self.dist.values())
@@ -163,15 +173,15 @@ class TextRank(Keyword, Graph):
         self.build_undirected_weighted_edges()
         old_score = dict()
         new_score = dict()
-        rank = len(self.items) / 3
+        rank = len(self.items) / 3 #use the top 1/3 words as the potential keywords
         for item in self.items:
             old_score[item] = self.c
 
         while True:
             for w1 in self.items:
                 in_sum = 0
-                for w2 in self.items:
-                    out_sum = sum(self.edges[(w2, w3)] for w3 in self.items if w3 != w1 and w3 != w2)
+                for w2 in self.neighbors[w1]:
+                    out_sum = sum(self.edges[(w2, w3)] for w3 in self.neighbors[w2] if w3 != w2)
                     if w2 != w1:
                         in_sum += (self.edges[(w1, w2)] * old_score[w2] / out_sum)
                 new_score[w1] = 1 - self.d + self.d * in_sum
@@ -184,20 +194,58 @@ class TextRank(Keyword, Graph):
         self.kw = [item[0] for item in temp][:rank]
 
 
+class Solution(object):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.extractors = {'textrank': TextRank(data_dir), 'closeness' : Closeness(data_dir)}
+        self.accuracy = {'textrank': 0.0, 'closeness' : 0.0}
+        self.q_accuracy = {'textrank': Queue(), 'closeness': Queue()}
+        self.filelist = filter(lambda filename: filename.endswith('txt'), os.listdir(self.data_dir))
+        self.q_filelist = Queue()
+        self.nfiles = len(self.filelist)
+        self.ncores = cpu_count()
+
+    def go_rank(self, key):
+        extractor = self.extractors[key]
+        while True:
+            if self.q_filelist.empty():
+                break
+            filename = self.q_filelist.get()
+            ans_file = filename[:-3]  + 'key'
+            extractor.fit(filename, ans_file)
+            self.q_accuracy[key].put(extractor.get_accuracy_recall()[0])
+
+    def get_accuracy(self, key):
+        for filename in self.filelist:
+            self.q_filelist.put(filename)
+        self.accuracy[key] = 0.0
+        processes = []
+        for _ in range(self.ncores):  #construct ncores of processes to run simutaneously
+            p = Process(target = self.go_rank, args = (key,))
+            p.start()
+            processes.append(p)
+        for process in processes:
+            process.join()
+        while not self.q_accuracy[key].empty():
+            self.accuracy[key] += self.q_accuracy[key].get()
+        self.accuracy[key] /= self.nfiles
+        return self.accuracy[key] 
+
+    def get_accurancy_from_closeness_rank(self):
+        return self.get_accuracy('closeness')
+
+    def get_accuracy_from_text_rank(self):
+        return self.get_accuracy('textrank')
+
+
 if __name__ == '__main__':
     data_dir = sys.argv[1]
-    C = Closeness(data_dir)
-    T = TextRank(data_dir)
+    solution = Solution(data_dir)
+    print solution.get_accuracy_from_text_rank()
+    print solution.get_accurancy_from_closeness_rank()
 
-    Extractor = [C, T]
-    accuracy = [0.0] * len(Extractor)
-    count = 0
 
-    for filename in os.listdir(data_dir):
-        if filename.endswith('txt'):
-            ans_file = filename[:-3] + 'key'
-            count += 1
-            for index, E in enumerate(Extractor):
-                E.fit(filename, ans_file)
-                accuracy[index] += E.get_accuracy_recall()[0]
-    accuracy = [accu / count for accu in accuracy]
+
+
+
+
