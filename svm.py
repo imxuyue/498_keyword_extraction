8b9984add934c0bd53ea3111a784b4a2bfd31780
Vinay Hiremath <vhiremath95@gmail.com>
2016-04-06 11:41:45 -0400

    Added SVM code

--- svm.py
+++ svm.py
@@ -1,9 +1,10 @@
 import numpy as np
+from sklearn.svm import SVC
 
 def train_svm(X, y):
-    clf = 0.0
+    clf = SVC()
+    clf.fit(X, y)
     return clf
 
 def test_svm(clf, X, y):
-    accuracy = 0.0
-    return accuracy
+    return clf.score(X, y)
