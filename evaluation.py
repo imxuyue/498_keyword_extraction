a7e40a71fdaf689cd674e1f7a3850f6c1181a3e4
Yue Xu <xuyue@umich.edu>
2016-04-06 23:34:36 -0400

    evaluation.py

---
+++ evaluation.py
@@ -0,0 +1,35 @@
+'''
+evaluation.py
+-------------
+precision, recall, etc
+'''
+from __future__ import division
+from feature_extraction import extract_features_test
+
+def get_precision(true_keys, pred_keys):
+    true_keys = [key.lower() for key in true_keys]
+    correct = len(set(true_keys) & set(pred_keys))
+    return correct / len(pred_keys)
+
+def get_recall(true_keys, pred_keys):
+    true_keys = [key.lower() for key in true_keys]
+    correct = len(set(true_keys) & set(pred_keys))
+    return correct / len(true_keys)
+
+def evaluate_on_each_doc(classifier, features_doc, labels_doc, phrase_idx_doc, phrase_list, true_keys_doc):
+    precisons = []
+    recalls = []
+    # go through each document
+    for features, labels, phrase_indices, true_keys in zip(features_doc, labels_doc, phrase_idx_doc, true_keys_doc):
+        pred_labels = classifier.predict(features)
+        pred_keys = []
+        # collect all phrases that has pred label 1
+        for label, idx in zip(pred_labels, phrase_indices):
+            if label == 1:
+                pred_keys.append(phrase_list[idx])
+        precisions.append(get_precision(true_keys, pred_keys))
+        recalls.append(get_recall(true_keys, pred_keys))
+
+    precision_avg = sum(precisions) / len(precisions)
+    recall_avg = sum(recalls) / len(recalls)
+    return precision_avg, recall_avg
